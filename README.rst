55cfceb54536b7036f8f84b9d20f6fe25764bba0
Janik von Rotz <login@janikvonrotz.ch>
2021-01-07 17:10:45 +0100

    init

---
+++ README.rst
@@ -0,0 +1,18 @@
+===========
+EKSB Layout
+===========
+
+.. image:: ./static/description/icon.png
+  :width: 100
+  :alt: Icon
+
+Layout customization for the EKSB project.
+
+Usage
+~~~~~
+
+Clone module into Odoo addon directory.
+
+.. code-block:: bash
+
+    git clone git@github.com:Mint-System/Odoo-App-EKSB-Layout.git ./addons/eksb_layout
---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "EKSB Layout",
+
+    'summary': """
+        Layout customization for the EKSB project.
+    """,
+
+    'description': """
+        Layout customization for the EKSB project.
+    """,
+
+    'author': "Mint System GmbH",
+    'website': "https://www.mint-system.ch",
+    'category': 'Accounting',
+    'version': '14.0.1.0.1',
+
+    'depends': ['base', 'web'],
+
+    'data': [
+        'views/external_layout_standard.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+}
